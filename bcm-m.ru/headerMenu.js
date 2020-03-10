80260b58ea64b35eb72ed7d062558f3ab52d0963
Dmitry <recc@tut.by>
2020-03-10 21:55:21 +0300

    Добавлено выпадающее меню

--- bcm-m.ru/headerMenu.js
+++ bcm-m.ru/headerMenu.js
@@ -1,16 +1,5 @@
-(function () {
-
-    let menuButton = document.querySelector(".menu_toggle");
-    let menuH = document.querySelector(".header .menu").clientHeight;
-    let menu = document.querySelector(".header .menu");
-    menuButton.addEventListener("click", toggleMenu);
-    function toggleMenu() {
-        menu.classList.toggle("active");
-        menu.style.height = "0";
-        console.log('test')
-        // let timer = setInterval(() => {
-        //     menu.style.height += 100 + "px";
-        // }, 1000)
-    }
-
-}());
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("show");   
+    console.log("test");
+    // $("#menu").toggleClass("show");
+}
