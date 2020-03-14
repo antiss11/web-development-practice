479945e9d53baf3fd50de64031d8fcb28c1bf089
Dmitry <recc@tut.by>
2020-03-14 10:52:33 +0300

    Готова основа слайдера

--- bcm-m.ru/sliderJQuery.js
+++ bcm-m.ru/sliderJQuery.js
@@ -1,21 +1,68 @@
 (function() {
     let sliderWidth = $(".we").width();       
-    let $sliderBody = $(".we_slider");
-    let currentSlide = 1;   
-    initialSlider();
-    $(window).resize( () => console.log("test"))
+    const $sliderBody = $(".we_slider");
+    const len = $("slide").length;
+    const animTime = 1000;
+
+    initialSlider();   
+    initialButtons(); 
     
     function initialSlider() {
-        let sliderWidth = getSliderWidth();  
-        $(".slide").css("transform", "translateX(-" + sliderWidth + "px)");
+        $(".slide").each(function() {
+            $(this).css("transform", "translateX(-" + sliderWidth + "px)");            
+        })
     }
 
-    function getSliderWidth() {
-        return $(".we").width()
+    function initialButtons() {
+        $(".slider_button.prev").click(prev);
+        $(".slider_button.next").click(next);
     }
 
+    
     function resizeCallback(currentSlideN) {
-        let sliderWidth = getSliderWidth();
-        $(".slide").css("position", "fixed");        
+        sliderWidth = $(".we").width();
+    }
+
+    function next() {      
+        let $slides = $(".slide");
+        let offset = sliderWidth;
+        $slides.each(function() {  
+            $(this).animate({
+                "left": offset
+            }, animTime)
+            offset += sliderWidth;
+        });
+        prependLastSlide()      
+    }
+
+    function prev() {      
+        let $slides = $(".slide");
+        let offset = -sliderWidth;
+        $slides.each(function() {  
+            $(this).animate({
+                "left": offset 
+            }, animTime)
+            offset += sliderWidth;
+        });
+        appendFistSlide(); 
+    }
+
+  
+    function prependLastSlide() {
+        let $lastSlide = $(".slide:last");      
+        let firstSlideOffset = $(".slide:first").offset();
+        firstSlideOffset.right -= sliderWidth; 
+        $lastSlide.finish();
+        $lastSlide.offset(firstSlideOffset);
+        $lastSlide.prependTo(".we_slider");
+    }
+
+    function appendFistSlide() {
+        let $firstSlide = $(".slide:first");      
+        let lastSlideOffset = $(".slide:last").offset();
+        lastSlideOffset.left += sliderWidth; 
+        $firstSlide.finish();
+        $firstSlide.offset(lastSlideOffset);
+        $firstSlide.appendTo(".we_slider");
     }
 })();
