ca7e578724bff80655d11ebf7193e5ccef2c5efe
Dmitry <recc@tut.by>
2020-06-19 21:54:41 +0300

    Добавлены aside, main

--- shop/sliderCode.js
+++ shop/sliderCode.js
@@ -1,27 +1,37 @@
 "use strict";
 
-const totalSlides = document.querySelectorAll(".slide").length - 1;
-let counter = 0;
-let nextButton = document.querySelector(".slider__button.next");
-let prevButton = document.querySelector(".slider__button.prev");
+(function() {
+    //    Код слайдера
+    const totalSlides = document.querySelectorAll(".slide").length - 1;
+    let counter = 0;
+    let nextButton = document.querySelector(".slider__button.next");
+    let prevButton = document.querySelector(".slider__button.prev");
 
-nextButton.onclick = nextSlide;
-prevButton.onclick = prevSlide;
+    nextButton.onclick = nextSlide;
+    prevButton.onclick = prevSlide;
 
-function nextSlide() {
-    counter++;
-    if (counter > totalSlides) {
-        counter = 0;
+    function nextSlide() {
+        counter++;
+        if (counter > totalSlides) {
+            counter = 0;
+        }
+        document.querySelector(".slide.active").classList.toggle("active");
+        document.querySelectorAll(".slide").item(counter).classList.toggle("active");
     }
-    document.querySelector(".slide.active").classList.toggle("active");
-    document.querySelectorAll(".slide").item(counter).classList.toggle("active");
-}
 
-function prevSlide() {
-    counter--;
-    if (counter < 0) {
-        counter = totalSlides;
+    function prevSlide() {
+        counter--;
+        if (counter < 0) {
+            counter = totalSlides;
+        }
+        document.querySelector(".slide.active").classList.toggle("active");
+        document.querySelectorAll(".slide").item(counter).classList.toggle("active");
     }
-    document.querySelector(".slide.active").classList.toggle("active");
-    document.querySelectorAll(".slide").item(counter).classList.toggle("active");
-}
+})();
+
+(function() {
+    // Код подменю
+    let isOpen = false;
+
+})();
+
