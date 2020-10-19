20ef60dc10f9e836e13e36ba939f45b50ddf9299
Dmitry Kulik <recc@tut.by>
2020-10-19 19:26:26 +0300

    Files moved

---
+++ altair/src/index.js
@@ -0,0 +1,33 @@
+import './normalize.css';
+import './styles.css';
+import './img/logo.svg'
+
+function initialSlider() {
+    let slides = $(".blog-posts__item");
+    let totalSlides = slides.length;
+    let slideWidth = slides.width();
+    let windowWidth = $(window).width();
+    let picturesInWindow = Math.floor(windowWidth / slideWidth);
+    let allSlidesWidth = 0;
+
+    // Calculate full width
+    slides.each(function (index) {
+        allSlidesWidth += $(this).width();
+        allSlidesWidth += parseInt($(this).css("margin-right"));
+        allSlidesWidth += parseInt($(this).css("margin-left"));
+    });
+    allSlidesWidth -= slideWidth * picturesInWindow;
+
+    $("#slider").slider({
+        step: 0.001,
+        slide: moveSlide,
+    });
+
+    function moveSlide(event, ui) {
+        slides.each(function (index) {
+            $(this).css("transform", `translateX(-${ui.value * allSlidesWidth / 100}px)`);
+        })
+    }
+}
+
+initialSlider()
